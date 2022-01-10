3bac25974ae6f54d1c47c635939d49586e1786a3
tosaporn <Ponddmj28>
2022-01-10 08:29:09 +0700

    fixed - build dockercompose

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -2,12 +2,10 @@
 
 #copy new jar to build location
 WORKSPACE=$PWD
-IMAGE=$IMAGE
-TAG=$TAG
 cp -f $WORKSPACE/java-app/target/*.jar $WORKSPACE/jenkins/build/
 
 echo "***************************"
 echo "** Building Docker Image **"
 echo "***************************"
-
+echo "$IMAGE $TAG"
 cd jenkins/build/ && docker-compose build --no-cache
