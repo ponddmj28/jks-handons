4ec1349a3f960b4c4eb557feaadb205a83e1978c
tosaporn <Ponddmj28>
2022-01-10 09:19:13 +0700

    fixed - chown owner

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -6,5 +6,7 @@ export IMAGE=$(sed -n '1p' /tmp/.auth)
 export TAG=$(sed -n '2p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
 
+chown remote_user /var/run/docker.sock
+
 docker login -u tosaporn -p $PASS
 cd /tmp && docker-compose up -d
