e9f12bd023c7c11cc66a97cafeb39c1c84d82d7f
tosaporn <Ponddmj28>
2022-01-10 09:22:04 +0700

    fixed - chownowener

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-PATH=$PATH
-
 export IMAGE=$(sed -n '1p' /tmp/.auth)
 export TAG=$(sed -n '2p' /tmp/.auth)
 export PASS=$(sed -n '3p' /tmp/.auth)
-
-chown remote_user /var/run/docker.sock
 
 docker login -u tosaporn -p $PASS
 cd /tmp && docker-compose up -d
